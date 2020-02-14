261972e8ecb48036ae3c7f84304ca0f50efca7cf
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-14 07:35:19 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,8 +3,8 @@ def my_collect(collection)
   i=0
   new_collection =[]
   while (i<collection.length)
-    new_collection << yield(array[i]) 
+    new_collection << yield(collection[i]) 
   end
-  
+  new_collection
 end
 
